dabfe41153943854510f67426273c895c0c61daa
Ubuntu <ubuntu@ip-172-31-35-129.ec2.internal>
2025-02-14 18:41:12 +0000

    Saved local changes before merge

--- cropsight/config/settings/local.py
+++ cropsight/config/settings/local.py
@@ -14,7 +14,7 @@ SECRET_KEY = env(
     default="5k4ZRTEE4VfcrmCnrraKeCtMYi3zknRD9tPcYeKf2c7FeNUOavBVBoQYMJ1fUWrL",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
+ALLOWED_HOSTS = ["*"]  # noqa: S104
 
 # CACHES
 # ------------------------------------------------------------------------------
